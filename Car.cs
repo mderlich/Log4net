332161c824c189d2edf325910d3d4517f3c0d1d0
Matias Derlich <matias.derlich@innsa.com>
2024-06-18 16:16:47 -0300

    ok

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using log4net;
+
+namespace log4netAdvanced
+{
+    class Car
+    {
+        // create a static logger field
+        private static ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public string Model { get; set; }
+        public int YearReleased { get; set; }
+        public Person Owner { get; set; }
+
+        public Car(string model, int yearReleased, Person owner)
+        {
+            Model = model;
+            YearReleased = yearReleased;
+            Owner = owner;
+
+            logger.Debug(String.Format("Created a car {0} at {1}", this, DateTime.Now));
+        }
+
+        public override string ToString()
+        {
+            return String.Format("[{0} ({1}), owned by {2}]", Model, YearReleased, Owner);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
+
+namespace log4netAdvanced
+{
+    class Program
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            // create 2 persons
+            var person1 = new Person("Jonh", "Gold");
+            var person2 = new Person("James", "Miller");
+            // create 2 cars
+            var car1 = new Car("Tesla Model S", 2020, person1);
+            var car2 = new Car("Tesla Model X", 2020, person2);
+            // logging
+            logger.Debug("Some debug log");
+            logger.Info(String.Format("Person1: {0}", person1));
+            logger.Info(String.Format("Car2: {0}", car2));
+            logger.Warn(String.Format("Warning accrued at {0}", DateTime.Now));
+            logger.Error(String.Format("Error accrued at {0}", DateTime.Now));
+            logger.Fatal(String.Format("Serious problem with car {0} accrued at {1}", car1, DateTime.Now));
+        }
+    }
+}
